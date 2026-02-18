7793ab8599fae9a3a51b7da1547f722da15e86ea
NamanRawat86 <tata37968@gmail.com>
2026-02-18 13:16:36 +0530

    Implement matrix input and diagonal sum calculation

---
+++ day14.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+    scanf("%d %d", &m, &n);
+    int mat[m][n];
+    for (int i=0; i<m; i++)
+        for (int j=0; j<n; j++)
+            scanf("%d", &mat[i][j]);
+
+    int sum = 0;
+    for (int i=0; i<m && i<n; i++)
+        sum += mat[i][i];
+
+    printf("%d\n", sum);
+    return 0;
+}
