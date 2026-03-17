20b8ae99333d6f7cb983f58021862c11ef3ae94c
NamanRawat86 <tata37968@gmail.com>
2026-03-17 21:05:35 +0530

    Create day35.c

---
+++ day35.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* front = NULL;
+struct Node* rear = NULL;
+
+void enqueue(int val) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->next = NULL;
+    if (rear == NULL) {
+        front = rear = newNode;
+    } else {
+        rear->next = newNode;
+        rear = newNode;
+    }
+}
+
+void dequeue() {
+    if (front == NULL) {
+        printf("Queue Underflow\n");
+        return;
+    }
+    struct Node* temp = front;
+    front = front->next;
+    if (front == NULL) rear = NULL;
+    free(temp);
+}
+
+void display() {
+    struct Node* temp = front;
+    while (temp) {
+        printf("%d", temp->data);
+        if (temp->next) printf(" ");
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        int val;
+        scanf("%d", &val);
+        enqueue(val);
+    }
+
+    display();
+    return 0;
+}
