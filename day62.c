4c767f11b7461f5e77ae3179b5c11b8756811534
NamanRawat86 <tata37968@gmail.com>
2026-04-13 11:05:48 +0530

    Create day62.c

---
+++ day62.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+int main() {
+    int n, m, directed;
+    scanf("%d %d", &n, &m);
+    scanf("%d", &directed);
+
+    struct Node* adj[n];
+    for (int i = 0; i < n; i++) {
+        adj[i] = NULL;
+    }
+
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+
+        struct Node* newNode = createNode(v);
+        newNode->next = adj[u];
+        adj[u] = newNode;
+
+        if (!directed) {
+            newNode = createNode(u);
+            newNode->next = adj[v];
+            adj[v] = newNode;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("%d:", i);
+        struct Node* temp = adj[i];
+        while (temp) {
+            printf(" %d", temp->vertex);
+            temp = temp->next;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
